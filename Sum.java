b32d19169eeacdeb9d3f1ba8c9e1a757b45e8b18
Fai549 <>
2025-07-25 12:35:18 +0700

    first commitFai549

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Sum2 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.pintIn("Enter num1: ");
+        int num1 = scanner.nextInt(); 
+
+        System.out.pintIn("Enter num2: ");
+        int num2 = scanner.nextInt();
+
+        int sum=num1+num2;
+        System.out.pintIn("Sum = "+ sum +"of" + num1 +"and" + num2);            
+    }
+}
---
+++ VariablesExample.java
@@ -0,0 +1,19 @@
+public class VariablesExample {
+    public static void main (String args[]){
+        char clothingSize = 's';
+        int websiteVisitors = 0;
+        char studentGrade = 'A';
+        double studentGPA = 4.0;
+        final double PI = 3.14159;
+        float annualExpenses = 15000.00f;
+        final int MONTHS_IN_YEAR = 12;
+        System.out.println("clothing size: "+clothingSize);
+        System.out.println("Website Visitors: "+websiteVisitors);
+        System.out.println("Student Grade: "+studentGrade);
+        System.out.println("Student GPA: "+studentGPA);
+        System.out.println("Value of Pi: "+PI);
+        System.out.println("Annual Expenses: "+annualExpenses);
+        System.out.println("Months in a Year: "+MONTHS_IN_YEAR);
+    }
+    
+}
