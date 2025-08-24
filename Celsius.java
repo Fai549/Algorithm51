bf061470b246478cf78c21462885e5190166e390
Fai549 <684259015@webmail.npru.ac.th>
2025-08-24 17:34:15 +0700

    first

--- Celsius.java
+++ Celsius.java
@@ -1,4 +1,5 @@
 public class Celsius{
+<<<<<<< HEAD
     public static void main(String args[]){
      Scanner scanner = new Scanner(System.in);
         
@@ -8,5 +9,15 @@ public class Celsius{
 
         float Celsius  = (5.0f/9) * (fahrenheit-32)
         System.out.pintIn("Celsius = " +Celsius);    
+=======
+    public static void main(String[]args)  {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter Fahrenheit= ");
+        float fahrenheit = scanner.nextFloat();
+        float x = fahrenheit-32;
+        float celsius = (x * 5) /9;
+        System.out.println("Celsius = " +celsius);
+>>>>>>> f1d9fb2 (First commit)
     }
 }
---
+++ EvereageSum.java
@@ -0,0 +1,26 @@
+public class EvereageSum{
+    public statiic void min(String args[]){
+        Scanner scanner - new Scanner (System.in);
+
+        System.out.priInt("Enter Num1 : ");
+        int num1 = scanner.nextInt();
+
+        System.out.priInt("Enter Num2 : ");
+        int num2 = scanner.nextInt();
+
+        System.out.priInt("Enter Num3 : ");
+        int num3 = scanner.nextInt();
+
+        System.out.priInt("Enter Num4 : ");
+        int num4 = scanner.nextInt();
+
+        System.out.priInt("Enter Num5 : ");
+        int num5 = scanner.nextInt();
+
+        int sum = num1+num2+num3+num4+num5;
+        int ever = sum/5;
+        System.out.printIn("rotal is "+sum+"Everage is "+ever);
+   } 
+}
+   
+    
